f89231da1402845307e0efa415eb310fcbf34201
evershinemj <37256562+evershinemj@users.noreply.github.com>
2019-07-29 22:27:12 +0800

    modified Happy.java

--- Happy.java
+++ Happy.java
@@ -1,5 +1,7 @@
 public class Happy {
     public static void main(String[] args) {
+        // edited on Github
+        System.out.println("Very happy");
         System.out.println("I'm quite happy now");
     }
 }
