a77587f5c7aa11e2efd3343779760d76d2a210b8
wangxueming <vndxmg@cn.ibm.com>
2019-08-01 10:08:50 +0800

    should use import static for org.junit.Assert.*

--- Happy.java
+++ Happy.java
@@ -1,7 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class Happy {
 	public void showHappiness() {
 		// happy
 		System.out.println("extremely happy now!");
 		System.out.println("though very hot today");
+	}
+
+	@Test
+	public void testTrue() {
+		assertTrue(true);
 	}
 }
