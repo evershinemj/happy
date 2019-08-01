58c4066208bed47664bcb178b24bd1e8e9a7cd92
wangxueming <vndxmg@cn.ibm.com>
2019-08-01 11:34:02 +0800

    revert

--- Happy.java
+++ Happy.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.*;
 
 public class Happy {
 	public void showHappiness() {
-		// happy
 		System.out.println("extremely happy now!");
 		System.out.println("though very hot today");
 	}
