dd8e2549f07e16b42d2a01193398f76f15d663d9
wangxueming <459226479@qq.com>
2019-08-09 22:02:29 +0800

    typhoon today

--- Happy.java
+++ Happy.java
@@ -1,6 +1,26 @@
+// <<<<<<< HEAD
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Happy {
 	public void showHappiness() {
-		// happy
 		System.out.println("extremely happy now!");
+		System.out.println("though very hot today");
+		System.out.println("typhoon today");
 	}
+
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+// =======
+/*
+public class Happy {
+    public static void main(String[] args) {
+        // edited on Github
+        System.out.println("Very happy");
+        System.out.println("I'm quite happy now");
+    }
+*/
+// >>>>>>> origin/master
 }
