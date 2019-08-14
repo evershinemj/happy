3de9b7bf1cd54eb55a7e41d1d433d40408a8719a
wangxueming <459226479@qq.com>
2019-08-14 23:43:31 +0800

    On branch foo 	modified:   Happy.java

--- Happy.java
+++ Happy.java
@@ -3,6 +3,8 @@ public class Happy {
         System.out.println("I'm quite happy now");
         // on branch foo
         System.out.println("wanna eat ice-cream");
+        // on branch foo
+        System.out.println("sleepy");
         // on branch master
         System.out.println("hot now");
         // on branch master
