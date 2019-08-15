bfe35bca9b77c0aa65d9f45b2aaa72fd3e6dba08
WilliamSummer <vndxmg@cn.ibm.com>
2019-08-15 09:49:47 +0800

    On branch master 	modified:   Happy.java

--- Happy.java
+++ Happy.java
@@ -2,6 +2,7 @@ public class Happy {
 	public void showHappiness() {
 		System.out.println("extremely happy now!");
 		// on branch foo
+		// on branch master
 		System.out.println("extremely happy now!");
 	}
 }
