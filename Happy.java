53814536a5032ca1d8cfcac271372fb1e94f06ea
wangxueming <459226479@qq.com>
2019-08-16 23:10:40 +0800

    Revert "Merge branch 'foo'"
    This reverts commit 980efd7ff4a2eb02b0b7deac36b9b23fa9ba7a03, reversing
    changes made to f89231da1402845307e0efa415eb310fcbf34201.

--- Happy.java
+++ Happy.java
@@ -1,20 +1,7 @@
 public class Happy {
-    /*
-     * on branch master
     public static void main(String[] args) {
         // edited on Github
         System.out.println("Very happy");
         System.out.println("I'm quite happy now");
     }
-    */
-
-	// on branch foo
-	public void showHappiness() {
-		// happy
-		System.out.println("extremely happy now!");
-	}
-
-	public static void main(String[] args) {
-		System.out.println("very happy");
-	}
 }
