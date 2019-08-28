5bd3b2cb26c8a05458e202d4014e2eaace33d452
evershinemj <37256562+evershinemj@users.noreply.github.com>
2019-08-28 22:49:47 +0800

    Update Happy.java

--- Happy.java
+++ Happy.java
@@ -1,8 +1,8 @@
-// <<<<<<< HEAD
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Happy {
+	
 	public void showHappiness() {
 		System.out.println("extremely happy now!");
 		System.out.println("though very hot today");
@@ -15,14 +15,4 @@ public class Happy {
 	public void testTrue() {
 		assertTrue(true);
 	}
-// =======
-/*
-public class Happy {
-    public static void main(String[] args) {
-        // edited on Github
-        System.out.println("Very happy");
-        System.out.println("I'm quite happy now");
-    }
-*/
-// >>>>>>> origin/master
 }
