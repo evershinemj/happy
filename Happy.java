e21195fa5d82d507c0f70333cd544b2889d33e8d
wangxueming <459226479@qq.com>
2019-09-21 13:33:22 +0800

    On branch master 	modified:   Happy.java

--- Happy.java
+++ Happy.java
@@ -1,4 +1,3 @@
-// <<<<<<< HEAD
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -6,37 +5,10 @@ public class Happy {
 	public void showHappiness() {
 		System.out.println("extremely happy now!");
 		System.out.println("though very hot today");
-		System.out.println("so hot today!");
 	}
 
 	@Test
 	public void testTrue() {
 		assertTrue(true);
 	}
-// =======
-/*
-public class Happy {
-    public static void main(String[] args) {
-        // edited on Github
-        System.out.println("Very happy");
-        System.out.println("I'm quite happy now");
-        // on branch foo
-        System.out.println("wanna eat ice-cream");
-        // on branch foo
-        System.out.println("sleepy");
-        // on branch foo
-        System.out.println("late now");
-        // on branch foo
-        System.out.println("go to bed");
-        // on branch master
-        System.out.println("hot now");
-        // on branch master
-        System.out.println("really hot");
-        // on branch master
-        System.out.println("really sleepy");
-        // on branch master
-        System.out.println("time to go to bed");
-    }
-*/
-// >>>>>>> origin/master
 }
